71e3504432d33bf788d1ca576309040a953eaa35
alexgg9 <alexgalvezgarciaqt@gmail.com>
2023-11-02 10:35:52 +0100

    nota

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit{
+  
+    @Input('clase') clase:string = 'btn btn-primary';
+    @Input('texto') texto:string = 'Botón';
+    @Output() action = new EventEmitter();
+
+    constructor() { }
+    
+    ngOnInit(): void {}
+}
